51565b44557d97f2e654786eda38b9b237d7eb1a
vunghiak3 <vunghiak3@gmail.com>
2024-06-18 18:38:59 +0700

    feat: add features movement to monster

---
+++ Assets/Scripts/enemyAi.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class emenyAI : MonoBehaviour
+{
+    public NavMeshAgent ai;
+    public List<Transform> destinations;
+    public Animator aiAnim;
+    public float walkSpeed, chaseSpeed, minIdleTime, maxIdleTime, idleTime;
+    public bool walking, chasing;
+    public Transform player;
+    Transform currentDest;
+    Vector3 dest;
+    int randNum, randNum2;
+    //public int destinationAmount;
+
+    void Start()
+    {
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("waypoint");
+        foreach (GameObject waypoint in waypoints)
+        {
+            destinations.Add(waypoint.transform);
+        }
+
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+        aiAnim.SetTrigger("walk");
+    }
+
+    void Update()
+    {
+        if (walking == true)
+        {
+            dest = currentDest.position;
+            ai.destination = dest;
+            ai.speed = walkSpeed;
+
+            if (!ai.pathPending && ai.remainingDistance <= ai.stoppingDistance)
+            {
+                randNum2 = Random.Range(0, 2);
+                if (randNum2 == 0)
+                {
+                    randNum = Random.Range(0, destinations.Count);
+                    currentDest = destinations[randNum];
+                }
+                if (randNum2 == 1)
+                {
+                    aiAnim.ResetTrigger("walk");
+                    aiAnim.SetTrigger("idle");
+                    StartCoroutine(stayIdle());
+                    walking = false;
+                }
+            }
+        }
+    }
+
+    IEnumerator stayIdle()
+    {
+        idleTime = Random.Range(minIdleTime, maxIdleTime);
+        yield return new WaitForSeconds(idleTime);
+        walking = true;
+        randNum = Random.Range(0, destinations.Count);
+        currentDest = destinations[randNum];
+        aiAnim.ResetTrigger("idle");
+        aiAnim.SetTrigger("walk");
+    }
+}
